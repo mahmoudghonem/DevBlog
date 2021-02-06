c7de1f4bbcee4b60533430a9abadbbcbcdcd7fcb
Mahmoud Ghonem <mahmoudghonem28@gmail.com>
2021-02-06 23:31:48 +0200

    add cors

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const express = require('express');
 const path = require("path");
 const routes = require('./routes')
 const errorHandler = require('./helper/errorHandler');
-
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 80;
 
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    next();
-});
+// app.use(function (req, res, next) {
+//     //Enabling CORS
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+//     next();
+// });
 // parse requests of content-type - application/json
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }))
-
+app.use(cors());
 app.use("/images", express.static(path.join("images")));
 
 app.use('/', routes);

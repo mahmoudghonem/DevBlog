bcc565b3306ab4b2e466c69512503a09b2b0cbb2
Mahmoud Ghonem <mahmoudghonem28@gmail.com>
2021-02-07 01:01:24 +0200

    still image

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }))
 app.use(cors());
-app.use("/images", express.static(path.join("images")));
+app.use("./public/uploads", express.static(path.join("images")));
 
 app.use('/', routes);
 
